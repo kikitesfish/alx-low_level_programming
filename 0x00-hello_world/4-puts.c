8fffcb0d6be2beaad7bf983ddf5a1b1443f64bd5
kikitesfish <kikitesfish6@gmail.com>
2022-09-08 10:30:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
+
 /**
  *
  * * main - main function
  *
  * * Return: everytime 0
  *
- * */
+ * 
+ */
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
