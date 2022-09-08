85476c456b8d84280e5922940bc3b8e153ee70ab
kikitesfish <kikitesfish6@gmail.com>
2022-09-08 11:05:56 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main print function
+ *
+ *
+ * Return: 0 every time
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
