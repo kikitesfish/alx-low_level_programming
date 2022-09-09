616f1e8a25c54fad8fa1aba2dd3a4df438c8ad1e
kikitesfish <kikitesfish6@gmail.com>
2022-09-09 14:09:21 -0700

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0': y <= '9': y++)
+		{
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
