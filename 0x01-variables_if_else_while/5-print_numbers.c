a6f7e4950c482ddc64f7d80ebbadceaf640323bc
kikitesfish <kikitesfish6@gmail.com>
2022-09-09 11:44:26 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - number
+ *
+ * Return: always 0
+ */
+int main(void)
+
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%i", x);
+	}
+
+	printf('\n');
+	
+	return (0);
+}
