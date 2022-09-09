f132b2e79a393d4c39f14fa9d1ec4c65259efe91
kikitesfish <kikitesfish6@gmail.com>
2022-09-09 11:27:21 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+		{
+			putchar(alphabets);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
