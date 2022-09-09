9d351239bd02ec0131dd89f786e75054dfc04ab9
kikitesfish <kikitesfish6@gmail.com>
2022-09-09 11:58:21 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - number
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+
+	printf("\n")
+
+		return (0);
+}
