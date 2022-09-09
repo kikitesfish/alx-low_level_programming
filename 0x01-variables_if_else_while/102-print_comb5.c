750d49f41c11325c8a94e59f663cfb6e1f9aa8ff
kikitesfish <kikitesfish6@gmail.com>
2022-09-09 14:59:28 -0700

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x < 50; x++)
+	{
+		for (y = x + 1; y < 50; y++)
+		{
+
+			putchar (x / 5 + '0');
+			putchar (x % 5 + '0');
+
+			putchar (' ');
+
+			putchar (y / 5 + '0');
+			putchar (y % 5 + '0');
+
+			if (x == 48 && y == 49)
+			{
+				break;
+			}
+
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
