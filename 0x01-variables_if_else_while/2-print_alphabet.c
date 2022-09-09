aeca3c7073f226cee56fcd2c1ae6d7c364b9851e
kikitesfish <kikitesfish6@gmail.com>
2022-09-09 10:47:10 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,15 @@
  * Return: always 0
  */
 int main(void)
+
 {
 	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
-	putchar('\n');
 
+	putchar('\n');
 	return (0);
 }
