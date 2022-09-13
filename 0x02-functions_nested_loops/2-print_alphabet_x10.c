52cdad3a9e2d8ec0009f4e967a7cdc7611c9b454
kikitesfish <kikitesfish6@gmail.com>
2022-09-13 05:29:14 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 0
+		char y;
+
+	while (x <= 9)
+	{
+		for (y = 'a'; x <= 'z'; y++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		y++;
+
+	}
