589e7d929569c2a96d278d4f493e7c938b0bf90a
kikitesfish <kikitesfish6@gmail.com>
2022-09-13 06:57:11 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,21 +5,18 @@
  *
  * Return: always 0
  */
-
 void print_alphabet_x10(void)
 {
-	int x = 0;
+	int x = 0
 	while (x < 10)
 	{
 		char y = 'a';
 		while (y <= 'z')
 		{
-			_putchar(y);
+			_putchar(a);
 			y++;
 		}
-
 		_putchar('\n');
-
 		x++;
 	}
 }
