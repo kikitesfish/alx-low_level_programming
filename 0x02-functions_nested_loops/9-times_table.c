d2fffd0aa2015d0acf3e2744dfbb33840d6a00a4
kikitesfish <kikitesfish6@gmail.com>
2022-09-13 10:20:13 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - time table
+ *
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; a <= 9; att)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			d = c / 10;
+			e = c % 10;
+
+			if (b == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(e + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(d + '0');
+				_putchar(ei# ii + '0');
+			}
+		}
+		_putchat('\n');
+	}
+}
