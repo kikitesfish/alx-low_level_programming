add2cd762585cc0aa66278eacda1793761f994b8
kikitesfish <kikitesfish6@gmail.com>
2022-09-13 13:10:35 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = 0, b = 1, c = 0 d = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0)
+			d += c;
+	}
+	printf("%i\n", d);
+	return (0);
+}
