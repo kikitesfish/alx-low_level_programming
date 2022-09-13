57014918cd29451fe7536e88e5db530bd23f52ac
kikitesfish <kikitesfish6@gmail.com>
2022-09-13 14:47:09 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,4 +8,38 @@
 
 int main(void)
 {
+	unsigned long i;
+	unsigned long j;
+	unsigned long k;
+	unsigned long l = 1000000000;
+	unsigned long m;
+	unsigned long m2;
+	unsigned long n1;
+	unsigned long n2;
 
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", k);
+		k += j;
+		j = k - j;
+	}
+
+	m = (j / l);
+	m2 = (j % l);
+	n1 = (k / l);
+	n2 = (k % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", n1 + (n2 / l));
+		printf("%lu", n2 % l);
+		n1 = n1 + m;
+		m = n1 - m;
+		n2 = n2 + m2;
+		m2 = n2 - m2;
+	}
+	printf ("\n");
+	return (0);
+}
