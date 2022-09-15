93bb2616faee8e030f3787cc4b4481c4413ba50f
kikitesfish <kikitesfish6@gmail.com>
2022-09-15 13:04:27 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _insupper - evaluet letter
+ * @c: print int
+ * Return: always o
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
