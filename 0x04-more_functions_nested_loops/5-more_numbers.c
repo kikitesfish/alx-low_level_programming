2c46bddc05d51b23e2d35d5b1f6753ff24cc5712
kikitesfish <kikitesfish6@gmail.com>
2022-09-15 13:54:54 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.c"
+#include <stdio.h>
+
+/**
+ * more_numbers - print numbers between 0 to 14
+ *
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		x = 0;
+		while (x <= 14)
+		{
+			if (x >= 10)
+			{
+				_putchar(x / 10 + '0');
+				_putchar(x % 10 + '0');
+			}
+			i++;
+		}
+		_putchar('\n');
+	}
+}
