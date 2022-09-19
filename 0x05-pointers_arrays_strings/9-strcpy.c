6601aa7d57525426d13d53c2852aac943fc42cb8
kikitesfish <kikitesfish6@gmail.com>
2022-09-19 11:56:05 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copie a sting from one aray to another
+ * @dest: a pointer to the destination
+ * @src: a pointer to the string
+ * Return: dest
+ */
+
+char *_sstrcpy(char *dest, char *src)
+{
+	int i;
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
