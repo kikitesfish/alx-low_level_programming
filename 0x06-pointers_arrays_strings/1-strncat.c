e3fec4b1aef6c96fadce9af4d75c168f03eed507
kikitesfish <kikitesfish6@gmail.com>
2022-09-22 16:00:33 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,23 +10,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int x, y;
-	
-	x = 0;
-	while (dest[x] != '\0')
-	{
-		x++;
-	}
-	
-	y = 0;
-	while (y < n && src[y] != '\0')
-	{
-		dest[x] = src[y];
-		y++;
-		x++;
-	}
-	
-	dest[x] = '\0';
-	
+	int ind = 0, dest_len = 0;
+
+	while (dest[ind++])
+		dest_len++;
+	for (ind = 0; src[ind] && ind < n; ind++)
+		dest[dest_len++] = src[ind];
 	return (dest);
 }
