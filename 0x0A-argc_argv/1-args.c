165a162b8052e2d0fe4fe3871d990b2f72bb46a0
kikitesfish <kikitesfish6@gmail.com>
2022-09-30 06:21:04 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
