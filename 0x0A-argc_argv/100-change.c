1df6be7a5b129a301c1b153f5488df8ff1e39320
kikitesfish <kikitesfish6@gmail.com>
2022-09-30 11:27:37 -0700

    commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,49 +1,77 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 /**
- * main -  minimum number of coins to make change for an amount of money
- * @argc: length
- * @argv: number
+ * coin_conv - coin converter
+ * @i: intiger
  *
- * Return: 0
+ * Return: the number of coins
+ */
+
+int coin_conv(int i)
+{
+	int count = 0;
+
+	while (i != 0)
+	{
+		if (i % 10 == 9 || i % 10 == 7)
+			i -= 2;
+		else if (i % 25 == 0)
+			i -= 25;
+		else if (i % 10 == 0)
+			i -= 10;
+		else if (i % 5 == 0)
+			i -= 5;
+		else if (i % 2 == 0)
+		{
+			if (i % 10 == 6)
+				i -= 1;
+			else
+				i -= 2;
+		}
+		else
+			i -= 1;
+
+		count ++;
+	}
+
+	return (count);
+}
+
+/**
+ * main - the minimum number of coins to make change for an amount of money
+ * @argc: number argument
+ * @argv: arguments
+ *
+ * Return: 0 or 1
  */
 
 int main(int argc, char *argv[])
 {
-	int a, b, c, d;
-	int coins[] = {25, 10, 5, 2, 1};
+	int i, coin;
 
-	a = b = c = d
+	coin = 0;
 
-		if (argc != 2)
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-	b = atoi(argv[1]);
-
-	if (b <= 0)
+	if (argc != 2)
 	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+
+	if (i < 0)
 		printf("0\n");
-		return (0);
-	}
 
-	while (coins[a] != '\0')
-
+	else
 	{
-		if (b >= coins[a])
+		coin = coinConverter(i);
 
-	{
-		aux = (b / coins[a]);
-		c += d;
-		b -= coins[a] * aux;
+		printf("%d\n", coin);
 	}
 
-	a++;
-	}
-
-	printf("%d\n", c);
 	return (0);
 }
