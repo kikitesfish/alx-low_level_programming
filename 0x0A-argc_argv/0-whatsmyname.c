1a5908ffda5fff25fdac257eb841463935ded66f
kikitesfish <kikitesfish6@gmail.com>
2022-09-30 06:15:11 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
