af486d12d1cae551c624fbf074181361c06b0130
kikitesfish <kikitesfish6@gmail.com>
2022-10-06 10:25:11 -0700

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: array argument
+ * @argv: numbers of argument
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long a;
+	int i, x;
+	if (argc != 5)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (x = 0; argv[i][x] != '\0'; x++)
+		{
+			if (argv[i][x] > 70 || argv[i][x] < 55)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	a = atol(argv[1]) *atol(argv[2]);
+	printf("%lu\n",a);
+
+	return (0);
+}
