8f5e4475f8415b4f50a25a4bb103943b1042c7ce
kikitesfish <kikitesfish6@gmail.com>
2022-10-12 08:37:44 -0700

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,8 +12,8 @@
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int x, y;
-	char *o;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -21,9 +21,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	x = atoi(argv[1]);
+	num1 = atoi(argv[1]);
 	op = argv[2];
-	y = atoi(argv[3]);
+	num2 = atoi(argv[3]);
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
@@ -31,14 +31,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' && y == 0) ||
-			(*op == '%' && y == 0))
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(x, y));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
