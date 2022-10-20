35e37b31de59b04c23381bb1206f8955183ad97e
kikitesfish <kikitesfish6@gmail.com>
2022-10-20 09:10:54 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: The linked list_t list
+ * Return: number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t element;
+
+	element = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		element++;
+	}
+	return (element);
+}
