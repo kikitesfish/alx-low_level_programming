60605644e6a36e714b3bdda0f0892ac638cf49f9
kikitesfish <kikitesfish6@gmail.com>
2022-10-31 08:20:49 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int x, y, z = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (z = 0; text_content[x];)
-			z++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	x = open((filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	y = write(x, text_content, z);
-	
-	if (x == -1 || w == -1)
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-		
-	close(x);
+
+	close(o);
 
 	return (1);
 }
+
